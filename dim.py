9354b7576d03135c527fd1226cd03847886b74e6
ankeshanand <ankeshanand1994@gmail.com>
2019-02-07 22:12:25 -0500

    Bunch of bug fixes, get tensor shapes right, add comments to make the code readable

--- dim.py
+++ dim.py
@@ -3,15 +3,17 @@ import torch.nn as nn
 import numpy as np
 from a2c_ppo_acktr.utils import init
 from torch.utils.data import BatchSampler, RandomSampler
+import itertools
 
 
 class MIEstimator():
-    def __init__(self, encoder, feature_size=512, global_span=8, lr=5e-4, epochs=10):
+    def __init__(self, encoder, feature_size=512, global_span=8, lr=5e-4, epochs=10, device=torch.device('cpu')):
         super(MIEstimator, self).__init__()
 
+        self.device = device
         self.encoder = encoder
         self.feature_size = feature_size
-        self.discriminator = Discriminator(self.feature_size*2)
+        self.discriminator = Discriminator(self.feature_size*2).to(device)
         self.optimizer = torch.optim.Adam(list(self.encoder.parameters()) + list(self.discriminator.parameters()), lr=lr)
         self.loss_fn = nn.BCEWithLogitsLoss()
         self.global_span = global_span
@@ -23,21 +25,27 @@ class MIEstimator():
         Iteratively yield batches of data
         :return:
         """
+        # Convert to 2d list from 3d list
+        episodes = list(itertools.chain.from_iterable(episodes))
         # Only consider episodes with len >= global_span
         episodes = [x for x in episodes if len(x) >= self.global_span]
         total_steps = sum([len(e) for e in episodes])
+        # Episode sampler
         sampler = BatchSampler(RandomSampler(range(len(episodes)),
                                              replacement=True, num_samples=total_steps // self.global_span),
-                               self.mini_batch_size, drop_last=False)
+                               self.mini_batch_size, drop_last=True)
+        # Iteratively yield batches of data
         for indices in sampler:
             episodes_batch = [episodes[x] for x in indices]
             pos_obs_batch, neg_obs_batch = [], []
             for episode in episodes_batch:
-                start_idx, start_idx_neg = np.random.choice(len(episode)), np.random.choice(len(episode))
-                pos_obs_batch += episode[start_idx:start_idx+self.global_span] # Append
-                neg_obs_batch += episode[start_idx_neg:start_idx_neg+self.global_span]
+                start_idx, start_idx_neg = np.random.choice(len(episode)-self.global_span), \
+                                           np.random.choice(len(episode)-self.global_span)
+                pos_obs_batch.append(torch.stack(episode[start_idx:start_idx+self.global_span])) # Append
+                neg_obs_batch.append(torch.stack(episode[start_idx_neg:start_idx_neg+self.global_span]))
 
-            yield pos_obs_batch, neg_obs_batch
+            # shape: mini_batch_size * global_span * obs_shape
+            yield torch.stack(pos_obs_batch), torch.stack(neg_obs_batch)
 
     def maximize_mi(self, episodes):
         """
@@ -48,17 +56,33 @@ class MIEstimator():
             data_generator = self.data_generator(episodes)
             for batch in data_generator:
                 pos_obs_batch, neg_obs_batch = batch
+                # pass to encoder as (-1, obs_shape)
+                pos_features_batch = self.encoder(pos_obs_batch.view(-1, *pos_obs_batch.shape[2:]))
+                # reshape to mini_batch_size * global_span * feature_size to compute global features
+                pos_features_batch = pos_features_batch.view(self.mini_batch_size, self.global_span, -1)
 
-                pos_features_batch = self.encoder(pos_obs_batch)
-                global_features = torch.mean(pos_features_batch).unsqueeze(0).expand(self.mini_batch_size, -1)
+                # compute global features by taking mean around the global_span dimension,
+                # then expand as features_batch to concat with features_batch
+                global_features = torch.mean(pos_features_batch, dim=1, keepdim=True).expand_as(pos_features_batch)
                 pos_samples = torch.cat((pos_features_batch, global_features), dim=-1)
-                neg_features_batch = self.encoder(neg_obs_batch)
+                # reshape to (-1, feature_size*2)
+                pos_samples = pos_samples.view(-1, pos_samples.shape[-1])
+
+                # Do the same to get neg_samples
+                neg_features_batch = self.encoder(neg_obs_batch.view(-1, *neg_obs_batch.shape[2:]))
+                neg_features_batch = neg_features_batch.view(self.mini_batch_size, self.global_span, -1)
                 neg_samples = torch.cat((neg_features_batch, global_features), dim=-1)
-                target = torch.cat((torch.ones(self.mini_batch_size, 1), torch.zeros(self.mini_batch_size, 1)), dim=0)
+                neg_samples = neg_samples.view(-1, neg_samples.shape[-1])
+                assert neg_samples.shape == pos_samples.shape
+
+                # Create target tensor
+                target = torch.cat((torch.ones(self.mini_batch_size*self.global_span, 1),
+                                    torch.zeros(self.mini_batch_size*self.global_span, 1)), dim=0).to(self.device)
+                # Concatenate positive and negative samples along 0th dimension to get data to feed to the disc
                 samples = torch.cat((pos_samples, neg_samples), dim=0)
 
                 self.optimizer.zero_grad()
-                loss = self.loss_fn(self.discriminator(samples, target))
+                loss = self.loss_fn(self.discriminator(samples), target)
                 loss.backward()
                 self.optimizer.step()
 
