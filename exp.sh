88547d1b599f6a67b1c4146b7f5afdc1efea511b
ankeshanand <ankeshanand1994@gmail.com>
2019-03-11 18:56:35 -0400

    Minor change in the run script

--- exp.sh
+++ exp.sh
@@ -2,9 +2,9 @@
 python run_contrastive.py --env-name 'PongNoFrameskip-v4' --contrastive-lr 1e-3 --cuda-id 0 &
 python run_contrastive.py --env-name 'PongNoFrameskip-v4' --contrastive-lr 5e-3 --cuda-id 1 &
 python run_contrastive.py --env-name 'PongNoFrameskip-v4' --contrastive-lr 1e-4 --cuda-id 2 &
-python run_contrastive.py --env-name 'PongNoFrameskip-v4' --contrastive-lr 3e-4 --cuda-id 3 &
+python run_contrastive.py --env-name 'PongNoFrameskip-v4' --contrastive-lr 5e-4 --cuda-id 3 &
 wait
 python run_contrastive.py --env-name 'MontezumaRevenge-v4' --contrastive-lr 1e-3 --cuda-id 0 &
 python run_contrastive.py --env-name 'MontezumaRevenge-v4' --contrastive-lr 5e-3 --cuda-id 1 &
 python run_contrastive.py --env-name 'MontezumaRevenge-v4' --contrastive-lr 1e-4 --cuda-id 2 &
-python run_contrastive.py --env-name 'MontezumaRevenge-v4' --contrastive-lr 3e-4 --cuda-id 3 &
+python run_contrastive.py --env-name 'MontezumaRevenge-v4' --contrastive-lr 5e-4 --cuda-id 3 &
