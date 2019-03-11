2ea39cb7ef18204f0493d615b372c292e2bdc815
ankeshanand <ankeshanand1994@gmail.com>
2019-03-10 22:45:36 -0400

    Add util for visualizing activation maps, refactor to move some stuff to utils.py

--- utils.py
+++ utils.py
@@ -9,6 +9,10 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import random
+import numpy as np
+
+from a2c_ppo_acktr.envs import make_vec_envs
+from a2c_ppo_acktr.utils import get_vec_normalize
 
 
 def preprocess():
@@ -52,13 +56,67 @@ def calculate_accuracy(preds, y):
     return acc
 
 
-def save_model(model, save_dir, model_name):
-    pass
+def save_model(model, envs, save_dir, model_name, use_cuda):
+    save_path = os.path.join(save_dir)
+    try:
+        os.makedirs(save_path)
+    except OSError:
+        pass
+
+    # A really ugly way to save a model to CPU
+    save_model = model
+    if use_cuda:
+        save_model = copy.deepcopy(model).cpu()
+
+    save_model = [save_model,
+                  getattr(get_vec_normalize(envs), 'ob_rms', None)]
+
+    torch.save(save_model, os.path.join(save_path, model_name + ".pt"))
 
 
-def visualize_activation_maps(encoder, input_obs_batch):
+def evaluate_policy(actor_critic, envs, args, eval_log_dir, device):
+    eval_envs = make_vec_envs(
+        args.env_name, args.seed + args.num_processes, args.num_processes,
+        args.gamma, eval_log_dir, args.add_timestep, device, True)
+
+    vec_norm = get_vec_normalize(eval_envs)
+    if vec_norm is not None:
+        vec_norm.eval()
+        vec_norm.ob_rms = get_vec_normalize(envs).ob_rms
+
+    eval_episode_rewards = []
+
+    obs = eval_envs.reset()
+    eval_recurrent_hidden_states = torch.zeros(args.num_processes,
+                                               actor_critic.recurrent_hidden_state_size, device=device)
+    eval_masks = torch.zeros(args.num_processes, 1, device=device)
+
+    while len(eval_episode_rewards) < 10:
+        with torch.no_grad():
+            _, action, _, eval_recurrent_hidden_states = actor_critic.act(
+                obs, eval_recurrent_hidden_states, eval_masks, deterministic=True)
+
+        # Obser reward and next obs
+        obs, reward, done, infos = eval_envs.step(action)
+
+        eval_masks = torch.FloatTensor([[0.0] if done_ else [1.0]
+                                        for done_ in done])
+        for info in infos:
+            if 'episode' in info.keys():
+                eval_episode_rewards.append(info['episode']['r'])
+    print(" Evaluation using {} episodes: mean reward {:.5f}\n".
+          format(len(eval_episode_rewards),
+                 np.mean(eval_episode_rewards)))
+    eval_envs.close()
+    return eval_episode_rewards
+
+
+def visualize_activation_maps(encoder, input_obs_batch, wandb):
     scaled_images = input_obs_batch / 255.
-    fmap = F.relu(encoder.layer3(encoder.layer2(encoder.layer1(scaled_images)))).detach()
+    if encoder.__class__.__name__ == 'ImpalaCNN':
+        fmap = F.relu(encoder.layer3(encoder.layer2(encoder.layer1(scaled_images)))).detach()
+    elif encoder.__class__.__name__ == 'NatureCNN':
+        fmap = F.relu(encoder.main[4](F.relu(encoder.main[2](F.relu(encoder.main[0](input_obs_batch)))))).detach()
     out_channels = fmap.shape[1]
     # upsample and add a dummy channel dimension
     fmap_upsampled = F.interpolate(fmap, size=input_obs_batch.shape[-2:], mode='bilinear').unsqueeze(dim=2)
@@ -66,5 +124,5 @@ def visualize_activation_maps(encoder, input_obs_batch):
         fmap_grid = make_grid(fmap_upsampled[i], normalize=True)
         img_grid = make_grid([scaled_images[i]] * out_channels)
         plt.imshow(img_grid.cpu().numpy().transpose([1, 2, 0]))
-        plt.imshow(fmap_grid.cpu().numpy().transpose([1, 2, 0]), cmap='jet', alpha=0.7)
-        plt.savefig('act_map' + str(i) + '.png')
+        plt.imshow(fmap_grid.cpu().numpy().transpose([1, 2, 0]), cmap='jet', alpha=0.5)
+        wandb.log({'actmap': wandb.Image(plt, caption='Activation Map')})
