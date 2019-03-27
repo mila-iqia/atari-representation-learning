5d1a659a84fd72b187ac4d8f7ce3be44d1d104a0
ankeshanand <ankeshanand1994@gmail.com>
2019-03-26 20:07:55 -0400

    Use SGD with Momentum

--- scripts/run_contrastive.py
+++ scripts/run_contrastive.py
@@ -34,7 +34,7 @@ def main():
         'lr': args.lr,
         'mini_batch_size': args.batch_size,
         'linear': args.linear,
-        'optimizer': 'Adam',
+        'optimizer': 'SGD',
     }
     wandb.config.update(config)
 
--- src/appo.py
+++ src/appo.py
@@ -36,8 +36,8 @@ class AppoTrainer(Trainer):
         self.epochs = epochs
         self.mini_batch_size = mini_batch_size
         self.device = device
-        self.optimizer = torch.optim.Adam(list(self.classifier.parameters()) + list(self.encoder.parameters()),
-                                          lr=lr, eps=1e-5)
+        self.optimizer = torch.optim.SGD(list(self.classifier.parameters()) + list(self.encoder.parameters()),
+                                          lr=lr, momentum=0.9)
         self.loss_fn = nn.BCEWithLogitsLoss()
 
     def generate_batch(self, episodes):
