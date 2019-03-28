3c3639b658b60acf469dc3e9406f2640bb0358e9
eracah <ejracah@gmail.com>
2019-03-28 01:57:17 -0400

    added atari info wrapper and had it applied before making vecenvs

---
+++ src/atari.py
@@ -0,0 +1,56 @@
+from gym.envs.registration import register
+import gym
+from gym import spaces
+import numpy as np
+import os
+
+atari_dict = {"Pitfall": {"ram": dict(agent_x=97,agent_y=105),
+                                 "num_classes": {}},
+                  
+                  
+                  
+                  "PrivateEye": {"ram": dict(agent_x=63,agent_y=86),
+                                    "num_classes": {}}
+                 }
+
+class InfoWrapper(gym.Wrapper):
+    def step(self, action):
+        observation, reward, done, info = self.env.step(action)
+        return observation, reward, done, self.info(info)
+
+
+    def info(self):
+        raise NotImplementedError
+        
+    def labels(self):
+        raise NotImplementedError
+        
+class AtariWrapper(InfoWrapper):
+    
+    def __init__(self, env):
+        super().__init__(env)
+        env_name = self.env.spec.id
+        self.env_name = env_name.split("-")[0].split("No")[0]
+        
+        if self.env_name in atari_dict:
+            self.ram_dict = atari_dict[self.env_name]["ram"]
+            ncd = atari_dict[self.env_name]["num_classes"] 
+            if len(ncd) > 0:
+                self.nclasses_dict = ncd
+            else:
+                self.nclasses_dict = {k:256 for k in self.ram_dict.keys()}
+        
+        
+        
+           
+    def info(self, info):
+        if self.env_name  in atari_dict:
+            info["num_classes"] = self.nclasses_dict
+            label_dict = self.labels()
+            info["labels"] = label_dict
+        return info
+    
+    def labels(self):
+        ram = self.env.env.ale.getRAM()
+        label_dict = {k:ram[ind] for k,ind in self.ram_dict.items()}
+        return label_dict
--- src/envs.py
+++ src/envs.py
@@ -1,8 +1,62 @@
 import torch
 from baselines.common.vec_env import SubprocVecEnv, DummyVecEnv
-from a2c_ppo_acktr.envs import make_env, VecNormalize, VecPyTorch
+from a2c_ppo_acktr.envs import TimeLimitMask, MaskGoal, TransposeObs,TransposeImage,VecPyTorch, VecNormalize, VecPyTorchFrameStack
 from pathlib import Path
+import os
+import gym
+import numpy as np
+import torch
+from gym.spaces.box import Box
+from baselines import bench
+from baselines.common.atari_wrappers import make_atari, wrap_deepmind
+from src.atari import AtariWrapper
 
+
+def make_env(env_id, seed, rank, log_dir, allow_early_resets):
+    def _thunk():
+        if env_id.startswith("dm"):
+            _, domain, task = env_id.split('.')
+            env = dm_control2gym.make(domain_name=domain, task_name=task)
+        else:
+            env = gym.make(env_id)
+
+        is_atari = hasattr(gym.envs, 'atari') and isinstance(
+            env.unwrapped, gym.envs.atari.atari_env.AtariEnv)
+        if is_atari:
+            env = make_atari(env_id)
+            env = AtariWrapper(env)
+            
+
+        env.seed(seed + rank)
+
+        obs_shape = env.observation_space.shape
+
+        if str(env.__class__.__name__).find('TimeLimit') >= 0:
+            env = TimeLimitMask(env)
+
+        if log_dir is not None:
+            env = bench.Monitor(
+                env,
+                os.path.join(log_dir, str(rank)),
+                allow_early_resets=allow_early_resets)
+
+        if is_atari:
+            if len(env.observation_space.shape) == 3:
+                env = wrap_deepmind(env)
+        elif len(env.observation_space.shape) == 3:
+            raise NotImplementedError(
+                "CNN models work only for atari,\n"
+                "please use a custom wrapper for a custom pixel input env.\n"
+                "See wrap_deepmind for an example.")
+
+        # If the input has shape (W,H,3), wrap for PyTorch convolutions
+        obs_shape = env.observation_space.shape
+        if len(obs_shape) == 3 and obs_shape[2] in [1, 3]:
+            env = TransposeImage(env, op=[2, 0, 1])
+
+        return env
+
+    return _thunk
 def make_vec_envs(env_name, seed, num_processes, gamma=0.99, log_dir='~/tmp/',
                   device=torch.device('cpu'), allow_early_resets=False):
     
