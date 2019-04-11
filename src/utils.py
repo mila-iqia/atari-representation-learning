6ecda91ca7dbd73ee2b0930deac771469078a78d
eracah <ejracah@gmail.com>
2019-04-11 14:24:59 -0400

    added early stopping to utils

--- src/utils.py
+++ src/utils.py
@@ -216,6 +216,50 @@ class appendabledict(defaultdict):
             self.__getitem__(k).append(v)
 
 
+class EarlyStopping(object):
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, patience=7, verbose=False, name=""):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement. 
+                            Default: False
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.name
+
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''Saves model when validation loss decrease.'''
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), name+'_checkpoint.pt')
+        self.val_loss_min = val_loss  
+        
+        
+        
 def bucket_coord(coord, num_buckets, min_coord=0, max_coord=255, stride=1):
     # stride is how much a variable is incremented by (usually 1)
     try:
