2b020675a4d495370f6d026d942192cefbf62835
Evan Racah <ejracah@gmail.com>
2019-04-17 18:20:48 -0400

    edited run scripts

--- exp.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-python -m scripts.run_contrastive --env-name 'PongNoFrameskip-v4' --lr 1e-3 --cuda-id 0 &
-python -m scripts.run_contrastive --env-name 'PongNoFrameskip-v4' --lr 1e-4 --cuda-id 1 &
-python -m scripts.run_contrastive --env-name 'PongNoFrameskip-v4' --lr 5e-4 --cuda-id 2 &
-python -m scripts.run_contrastive --env-name 'PongNoFrameskip-v4' --lr 3e-4 --cuda-id 3 &
--- exp_scripts/run_gpu.sl
+++ exp_scripts/run_gpu.sl
@@ -1,6 +1,8 @@
 #!/bin/bash -l
 #SBATCH --gres=gpu
 #SBATCH -t 960
+#SBATCH -c 6
+#SBATCH --mem=100Gb
 module_name=$1
 shift
 python -m $module_name  $@
