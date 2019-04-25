751f610139ad3c1310b3db9cd2eabb3d66031f23
eracah <ejracah@gmail.com>
2019-04-24 23:11:02 -0400

    added run script for rl-pretrained

--- exp_scripts/run_all.py
+++ exp_scripts/run_all.py
@@ -4,15 +4,17 @@ base_cmd = "sbatch"
 ss= "exp_scripts/run_gpu.sl"
 module = "scripts.run_probe"
 args = [base_cmd, ss, module]
-args.append("--method supervised")
-args.append("--num-frame-stack 1")
-envs =  ["video_pinball"] 
-"""['asteroids', 'berzerk', 'boxing',
+args.append("--method pretrained-rl-agent")
+args.append("--probe-collect-mode atari-zoo")
+args.append('--zoo-algos apex')
+args.append('--zoo-tags 1B 400M final 10HR')
+
+envs =  ['asteroids', 'berzerk', 'boxing',
         'demon_attack', 'enduro', 'freeway', 'frostbite', 'hero', 
         'ms_pacman', 'pong', 'private_eye', 'qbert', 'riverraid', 
         'seaquest', 'solaris', 'space_invaders', 'venture', 'video_pinball', 
         'yars_revenge','breakout','pitfall','montezuma_revenge'
-        ]"""
+        ]
 
 
 suffix = "NoFrameskip-v4"
