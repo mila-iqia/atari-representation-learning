cef4c3196dcbfabe9116db61820d333f8c3831ac
Ankesh Anand <ankeshanand1994@gmail.com>
2019-05-02 18:56:58 -0400

    Merge the new spatio-temporal estimator (#35)
    * Add RandomCrop and make spatial-temporal estimator global-global
    
    * Relu after maxpool
    
    * Use CutOut, bring back final conv layer since GAP doesn't seem to be working
    
    * Fix seed set for cutout
    
    * Global -> Patch temporal estimator
    
    * Use second last conv layer for MI loss
    
    * Use both global<->patches and patches<->patches loss
    
    * Add Loss 3: Global at time t, f7 patches at time t-1
    
    * Revert "Add Loss 3: Global at time t, f7 patches at time t-1"
    
    This reverts commit 08a720f5d5374b14e5d6fb774f2e043e816b5baa.
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Revert "Update Dockerfile"
    
    This reverts commit 4664521a7c6ccc7668042d956b1cd756f9dbd44f.
    
    * Revert "Update Dockerfile"
    
    This reverts commit 06b06846959aa9df6c7ab0fc3529196bc4c8fa5c.
    
    * Revert "Update Dockerfile"
    
    This reverts commit 23e77f8ad2bec169be3ff4def0bb88ddb8922783.
    
    * Revert "Update Dockerfile"
    
    This reverts commit 5e8f9c780a446ce90629a8b7f88a3c1f84ad9a99.

--- src/spatio_temporal.py
+++ src/spatio_temporal.py
@@ -1,18 +1,22 @@
+import random
+
 import torch
 import os
 import torch
 import torch.nn as nn
 import numpy as np
 from torch.utils.data import RandomSampler, BatchSampler
-from .utils import calculate_accuracy
+from .utils import calculate_accuracy, Cutout
 from .trainer import Trainer
 from src.utils import EarlyStopping
+from torchvision import transforms
+import torchvision.transforms.functional as TF
 
 
 class Classifier(nn.Module):
-    def __init__(self, num_inputs, hidden_size=256, linear=False):
+    def __init__(self, num_inputs1, num_inputs2):
         super().__init__()
-        self.network = nn.Bilinear(num_inputs, num_inputs, 1)
+        self.network = nn.Bilinear(num_inputs1, num_inputs2, 1)
 
     def forward(self, x1, x2):
         return self.network(x1, x2)
@@ -24,20 +28,18 @@ class SpatioTemporalTrainer(Trainer):
         super().__init__(encoder, wandb, device)
         self.config = config
         self.mode = config['mode']
-        self.feature_sizes = {
-            'pcl': self.encoder.hidden_size * 2,
-            'tcl': self.encoder.hidden_size + 1,
-            'both': self.encoder.hidden_size * 2 + 1
-        }
         self.patience = self.config["patience"]
-        self.classifier = Classifier(32, linear=config['linear']).to(device) # n_channels = 32
+        self.classifier1 = Classifier(self.encoder.hidden_size, 128).to(device)  # x1 = global, x2=patch, n_channels = 32
+        self.classifier2 = Classifier(128, 128).to(device)
         self.epochs = config['epochs']
         self.batch_size = config['batch_size']
         self.device = device
-        self.optimizer = torch.optim.Adam(list(self.classifier.parameters()) + list(self.encoder.parameters()),
+        self.optimizer = torch.optim.Adam(list(self.classifier1.parameters()) + list(self.encoder.parameters()) +
+                                          list(self.classifier2.parameters()),
                                           lr=config['lr'], eps=1e-5)
         self.loss_fn = nn.BCEWithLogitsLoss()
         self.early_stopper = EarlyStopping(patience=self.patience, verbose=False, wandb=self.wandb, name="encoder")
+        self.transform = transforms.Compose([Cutout(n_holes=1, length=80)])
 
     def generate_batch(self, episodes):
         total_steps = sum([len(e) for e in episodes])
@@ -55,58 +57,81 @@ class SpatioTemporalTrainer(Trainer):
                 t, t_hat = 0, 0
                 t, t_hat = np.random.randint(0, len(episode)), np.random.randint(0, len(episode))
                 x_t.append(episode[t])
+
+                # Apply the same transform to x_{t-1} and x_{t_hat}
+                # https://github.com/pytorch/vision/issues/9#issuecomment-383110707
+                # Use numpy's random seed because Cutout uses np
+                # seed = random.randint(0, 2 ** 32)
+                # np.random.seed(seed)
                 x_tprev.append(episode[t - 1])
-                if self.mode == 'both':
-                    x_that.append(episode[t_hat - 1])
-                else:
-                    x_that.append(episode[t_hat])
+                # np.random.seed(seed)
+                x_that.append(episode[t_hat])
+
                 ts.append([t])
                 thats.append([t_hat])
-            yield torch.stack(x_t).to(self.device) / 255., torch.stack(x_tprev).to(self.device) / 255., torch.stack(x_that).to(self.device) / 255., \
-                  torch.Tensor(ts).to(self.device), torch.Tensor(thats).to(self.device)
+            yield torch.stack(x_t).to(self.device) / 255., torch.stack(x_tprev).to(self.device) / 255., \
+                  torch.stack(x_that).to(self.device) / 255., torch.Tensor(ts).to(self.device), \
+                  torch.Tensor(thats).to(self.device)
 
     def do_one_epoch(self, epoch, episodes):
-        mode = "train" if self.encoder.training and self.classifier.training else "val"
+        mode = "train" if self.encoder.training and self.classifier1.training else "val"
         epoch_loss, accuracy, steps = 0., 0., 0
+        accuracy1, accuracy2 = 0., 0.
         data_generator = self.generate_batch(episodes)
         for x_t, x_tprev, x_that, ts, thats in data_generator:
-            f_t, f_t_prev = self.encoder(x_t), self.encoder(x_tprev)
-            f_t_2, f_t_hat = self.encoder(x_t), self.encoder(x_that)
+            f_t_maps, f_t_prev_maps = self.encoder(x_t, fmaps=True), self.encoder(x_tprev, fmaps=True)
+            f_t_hat_maps = self.encoder(x_that, fmaps=True)
 
-            target = torch.cat((torch.ones_like(f_t[:,:,:,0]),
-                                torch.zeros_like(f_t[:,:,:,0])), dim=0).to(self.device)
+            # Loss 1: Global at time t, f5 patches at time t-1
+            f_t, f_t_prev = f_t_maps['out'], f_t_prev_maps['f5']
+            f_t_hat = f_t_hat_maps['f5']
+            f_t = f_t.unsqueeze(1).unsqueeze(1).expand(-1, f_t_prev.size(1), f_t_prev.size(2), self.encoder.hidden_size)
 
-            x1, x2 = torch.cat([f_t, f_t_2], dim=0), torch.cat([f_t_prev, f_t_hat], dim=0)
+            target = torch.cat((torch.ones_like(f_t[:, :, :, 0]),
+                                torch.zeros_like(f_t[:, :, :, 0])), dim=0).to(self.device)
+
+            x1, x2 = torch.cat([f_t, f_t], dim=0), torch.cat([f_t_prev, f_t_hat], dim=0)
             shuffled_idxs = torch.randperm(len(target))
             x1, x2, target = x1[shuffled_idxs], x2[shuffled_idxs], target[shuffled_idxs]
             self.optimizer.zero_grad()
-            loss = self.loss_fn(self.classifier(x1, x2).squeeze(), target)
+            loss1 = self.loss_fn(self.classifier1(x1, x2).squeeze(), target)
 
+            # Loss 2: f5 patches at time t, with f5 patches at time t-1
+            f_t = f_t_maps['f5']
+            x1_p, x2_p = torch.cat([f_t, f_t], dim=0), torch.cat([f_t_prev, f_t_hat], dim=0)
+            x1_p, x2_p = x1_p[shuffled_idxs], x2_p[shuffled_idxs]
+            loss2 = self.loss_fn(self.classifier2(x1_p, x2_p).squeeze(), target)
+
+            loss = loss1 + loss2
             if mode == "train":
                 loss.backward()
                 self.optimizer.step()
 
             epoch_loss += loss.detach().item()
-            preds = torch.sigmoid(self.classifier(x1, x2).squeeze())
-            accuracy += calculate_accuracy(preds, target)
+            preds1 = torch.sigmoid(self.classifier1(x1, x2).squeeze())
+            accuracy1 += calculate_accuracy(preds1, target)
+            preds2 = torch.sigmoid(self.classifier2(x1_p, x2_p).squeeze())
+            accuracy2 += calculate_accuracy(preds2, target)
+            accuracy = (accuracy1 + accuracy2) / 2.
             steps += 1
-        self.log_results(epoch, epoch_loss / steps, accuracy / steps, prefix=mode )
+        self.log_results(epoch, epoch_loss / steps, accuracy / steps, prefix=mode)
         if mode == "val":
             self.early_stopper(accuracy, self.encoder)
 
     def train(self, tr_eps, val_eps):
         # TODO: Make it work for all modes, right now only it defaults to pcl.
         for e in range(self.epochs):
-            self.encoder.train(), self.classifier.train()
+            self.encoder.train(), self.classifier1.train(), self.classifier2.train()
             self.do_one_epoch(e, tr_eps)
 
-            self.encoder.eval(), self.classifier.eval()
+            self.encoder.eval(), self.classifier1.eval(), self.classifier2.eval()
             self.do_one_epoch(e, val_eps)
 
             if self.early_stopper.early_stop:
                 break
-        torch.save(self.encoder.state_dict(), os.path.join(self.wandb.run.dir,  self.config['env_name'] + '.pt'))
+        torch.save(self.encoder.state_dict(), os.path.join(self.wandb.run.dir, self.config['env_name'] + '.pt'))
 
     def log_results(self, epoch_idx, epoch_loss, accuracy, prefix=""):
-        print("{} Epoch: {}, Epoch Loss: {}, {} Accuracy: {}".format(prefix.capitalize(), epoch_idx, epoch_loss, prefix.capitalize(), accuracy))
+        print("{} Epoch: {}, Epoch Loss: {}, {} Accuracy: {}".format(prefix.capitalize(), epoch_idx, epoch_loss,
+                                                                     prefix.capitalize(), accuracy))
         self.wandb.log({prefix + '_loss': epoch_loss, prefix + '_accuracy': accuracy})
