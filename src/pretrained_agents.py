46f38f55f601b5f44cb4070247c5a7f55dc17dd4
eracah <ejracah@gmail.com>
2019-07-23 17:09:32 -0400

    moved rollouts function to episodes

--- src/pretrained_agents.py
+++ src/pretrained_agents.py
@@ -1,8 +1,16 @@
-import numpy as np
-import torch
-
 from a2c_ppo_acktr import utils
 from a2c_ppo_acktr.envs import make_vec_envs as mve
+from collections import deque
+from itertools import chain
+import numpy as np
+import torch
+import wandb
+import time
+import os
+
+from aari.envs import make_vec_envs
+from src.utils import get_argparser
+from aari.utils import download_run
 
 
 def evaluate(actor_critic, env_name, seed, num_processes, eval_log_dir,
@@ -46,95 +54,7 @@ def evaluate(actor_critic, env_name, seed, num_processes, eval_log_dir,
     return np.mean(eval_episode_rewards)
 
 
-import argparse
-from collections import deque
-from itertools import chain
-import numpy as np
-import torch
-import wandb
-import time
-import os
-
-from aari.envs import make_vec_envs
-from src.utils import get_argparser
-
-checkpointed_steps_full = [10753536, 1076736, 11828736, 12903936, 13979136, 15054336, 1536, 16129536, 17204736,
-                           18279936,
-                           19355136, 20430336, 21505536, 2151936, 22580736, 23655936, 24731136, 25806336, 26881536,
-                           27956736,
-                           29031936, 30107136, 31182336, 32257536, 3227136, 33332736, 34407936, 35483136, 36558336,
-                           37633536,
-                           38708736, 39783936, 40859136, 41934336, 43009536, 4302336, 44084736, 45159936, 46235136,
-                           47310336,
-                           48385536, 49460736, 49999872, 5377536, 6452736, 7527936, 8603136, 9678336]
-
-checkpointed_steps_full_sorted = [1536, 1076736, 2151936, 3227136, 4302336, 5377536, 6452736, 7527936, 8603136, 9678336,
-                                  10753536, 11828736, 12903936, 13979136, 15054336, 16129536, 17204736, 18279936,
-                                  19355136, 20430336, 21505536, 22580736, 23655936, 24731136, 25806336, 26881536,
-                                  27956736, 29031936, 30107136, 31182336, 32257536, 33332736, 34407936, 35483136,
-                                  36558336, 37633536, 38708736, 39783936, 40859136, 41934336, 43009536, 44084736,
-                                  45159936, 46235136, 47310336, 48385536, 49460736, 49999872]
-
-
-def download_run(args, checkpoint_step):
-    api = wandb.Api()
-    runs = list(api.runs("curl-atari/pretrained-rl-agents-2", {"state": "finished",
-                                                               "config.env_name": args.env_name}))
-    run = runs[0]
-    filename = args.env_name + '_' + str(checkpoint_step) + '.pt'
-    run.files(names=[filename])[0].download(root='./trained_models_full/', replace=True)
-    print('Downloaded ' + filename)
-    return './trained_models_full/' + filename
-
-
-def get_ppo_rollouts(args, steps, checkpoint_step):
-    filepath = download_run(args, checkpoint_step)
-    while not os.path.exists(filepath):
-        time.sleep(5)
-
-    envs = make_vec_envs(args, args.num_processes)
-
-    actor_critic, ob_rms = \
-        torch.load(filepath, map_location=lambda storage, loc: storage)
-
-    episodes = [[[]] for _ in range(args.num_processes)]  # (n_processes * n_episodes * episode_len)
-    episode_labels = [[[]] for _ in range(args.num_processes)]
-    episode_rewards = deque(maxlen=10)
-
-    step = 0
-    masks = torch.zeros(1, 1)
-    obs = envs.reset()
-    entropies = []
-    for step in range(steps // args.num_processes):
-        # Take action using a random policy
-        with torch.no_grad():
-            obs, action, _, _, actor_features, dist_entropy = actor_critic.act(obs, None, masks, deterministic=False)
-        action = torch.tensor([envs.action_space.sample() if np.random.uniform(0, 1) < 0.2 else action[i]
-                               for i in range(args.num_processes)]).unsqueeze(dim=1)
-        entropies.append(dist_entropy.clone())
-        obs, reward, done, infos = envs.step(action)
-        for i, info in enumerate(infos):
-            if 'episode' in info.keys():
-                episode_rewards.append(info['episode']['r'])
-
-            if done[i] != 1:
-                episodes[i][-1].append(obs[i].clone())
-                if "labels" in info.keys():
-                    episode_labels[i][-1].append(info["labels"])
-            else:
-                episodes[i].append([obs[i].clone()])
-                if "labels" in info.keys():
-                    episode_labels[i].append([info["labels"]])
-
-    # Convert to 2d list from 3d list
-    episodes = list(chain.from_iterable(episodes))
-    # Convert to 2d list from 3d list
-    episode_labels = list(chain.from_iterable(episode_labels))
-    mean_entropy = torch.stack(entropies).mean()
-    return episodes, episode_labels, np.mean(episode_rewards), mean_entropy
-
-
-def get_ppo_representations(args, checkpoint_step):
+def get_ppo_representations(args, steps, checkpoint_step):
     # Gives PPO represnetations over data collected by a random agent
     filepath = download_run(args, checkpoint_step)
     while not os.path.exists(filepath):
@@ -154,10 +74,10 @@ def get_ppo_representations(args, checkpoint_step):
     episode_rewards = deque(maxlen=10)
     episode_features = [[[]] for _ in range(args.num_processes)]
 
-    step = 0
+
     masks = torch.zeros(1, 1)
     obs = envs.reset()
-    for step in range(args.probe_steps // args.num_processes):
+    for step in range(steps // args.num_processes):
         # Take action using a random policy
         if args.probe_collect_mode == 'random_agent':
             action = torch.tensor(
